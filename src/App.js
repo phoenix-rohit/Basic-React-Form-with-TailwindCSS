98d6baf809e054f3d5db40eee38c23f9661a182b
phoenix-rohit <rohitrane88888@gmail.com>
2024-08-01 17:47:12 +0530

    Intial Commit(Tailwind myConfig and React Setup)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./output.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Form />;
 }
 
-export default App;
+function Form() {
+  return <h1 className="text-center text-purple-800 text-xl">Man</h1>;
+}
